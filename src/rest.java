781ed039b45954413ee944f8c2ed7ca2b0604a94
Kamal <Kamal.ahmed@ou.edu>
2019-06-14 03:09:23 -0500

    java rest

---
+++ src/rest.java
@@ -0,0 +1,24 @@
+
+import entities.*;
+import java.util.*;
+import javax.ws.rs.*;
+import javax.json.JsonArray;
+import javax.ws.rs.GET;
+import javax.json.JsonObject;
+import javax.ws.rs.Path;
+
+public class Rest{
+    @GET
+    public JsonArray hello() {
+        return Json.createArrayBuilder().
+        add(hello("smart", 18)).
+        add(hello("nice", 42)).
+        build();
+    }
+public JsonObject hi(String name, int age) {
+    return Json.createObjectBuilder().
+    add("name", name).
+    add("age", age).
+    build();
+}
+}
